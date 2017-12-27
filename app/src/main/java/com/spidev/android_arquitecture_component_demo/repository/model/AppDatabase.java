0e993701f766cfc26a73f88190a71162d028a258
Bill Flores <bilizen3@gmail.com>
2017-12-27 00:51:07 -0500

    add class person Dao,Entity,AppDatabase

---
+++ app/src/main/java/com/spidev/android_arquitecture_component_demo/repository/model/AppDatabase.java
@@ -0,0 +1,30 @@
+package com.spidev.android_arquitecture_component_demo.repository.model;
+
+import android.arch.persistence.db.SupportSQLiteOpenHelper;
+import android.arch.persistence.room.Database;
+import android.arch.persistence.room.DatabaseConfiguration;
+import android.arch.persistence.room.InvalidationTracker;
+import android.arch.persistence.room.RoomDatabase;
+
+import com.spidev.android_arquitecture_component_demo.repository.model.dao.PersonDao;
+import com.spidev.android_arquitecture_component_demo.repository.model.entity.Person;
+
+/**
+ * Created by Bill on 26/12/2017.
+ */
+
+@Database(entities = {Person.class},version = 1,exportSchema = false)
+public abstract class AppDatabase extends RoomDatabase {
+
+    public abstract PersonDao PersonDao();
+
+    @Override
+    protected SupportSQLiteOpenHelper createOpenHelper(DatabaseConfiguration config) {
+        return null;
+    }
+
+    @Override
+    protected InvalidationTracker createInvalidationTracker() {
+        return null;
+    }
+}
---
+++ app/src/main/java/com/spidev/android_arquitecture_component_demo/repository/model/dao/PersonDao.java
@@ -0,0 +1,31 @@
+package com.spidev.android_arquitecture_component_demo.repository.model.dao;
+
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Delete;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.Query;
+
+import com.spidev.android_arquitecture_component_demo.repository.model.entity.Person;
+
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Bill on 27/12/2017.
+ */
+@Dao
+public interface PersonDao {
+
+    @Query("SELECT * FROM Person")
+    List<Person> getAll();
+
+    @Query("SELECT * FROM Person WHERE idPerson IN (:idPerson)")
+    List<Person> loadAllByIds(int idPerson);
+
+    @Insert
+    void insertAll(Person... person);
+
+    @Delete
+    void delete(Person person);
+}
